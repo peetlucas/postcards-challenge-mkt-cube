8083c8d1699da27c36d44786f7a511cb20980911
Peter Shitote <51503440+peetlucas@users.noreply.github.com>
2023-06-30 01:51:33 +0300

    Created sitemap with Spatie/laravel-sitemap and generated sitemap command in app/console with a kernel command to schedule for daily between 7am-8pm hourly. (#9)

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(App\Http\Controllers\PostcardController::class)->group(function () {
     Route::get('/', 'index')->name('postcards.index');
     Route::get('/postcards/{postcard}', 'show')->name('postcards.show');
+    Route::get('/sitemap.xml', 'PostcardController@index');
 });
 
 Route::middleware([
