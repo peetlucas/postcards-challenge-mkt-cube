80ca331ca639c1ee16675b96646e05f01bd85840
pshitote@gmail.com <pshitote@gmail.com>
2023-06-30 02:57:00 +0300

    Modified dashboard to include link to manage postcards for add,edit and delete. Also, deny access to /dashboard and /register in robots.txt

--- app/Http/Controllers/PostcardController.php
+++ app/Http/Controllers/PostcardController.php
@@ -6,16 +6,31 @@ use App\Http\Requests\StorePostcardRequest;
 use App\Http\Requests\UpdatePostcardRequest;
 use App\Models\Postcard;
 
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\abort;
+use Illuminate\Support\Facades\Redirect;
+
+use Carbon\Carbon;
+
 class PostcardController extends Controller
 {
     /**
      * Display a listing of the resource.
-     */
+     * 
+     */   
     public function index()
     {
+        $isDraft = 0;
         return view('postcards.index', [
-            'postcards' => Postcard::paginate(20)
-        ]);
+
+            'postcards' => Postcard::latest()->filter(request(['search']))
+                    ->where('is_draft', '=', $isDraft)
+                    ->where((Carbon::parse(date('Y-m-d H:s:i', strtotime('online_at')))
+                    ->diffInSeconds(Carbon::parse(date('Y-m-d H:s:i', strtotime('offline_at'))), false)), '>=', '0')
+                    ->paginate(5)
+        ]);         
     }
 
     /**
@@ -23,15 +38,32 @@ class PostcardController extends Controller
      */
     public function create()
     {
-        //
+        return view('postcards.create');
+    }
+    
+    // Store Postcard Data
+    public function store(Request $request) {
+        $formFields = $request->validate([
+            'title' => 'required',            
+            'price' => 'required',           
+            'is_draft' => 'required'            
+        ]);    
+
+        if($request->hasFile('photo')) {
+            $formFields['photo'] = $request->file('photo')->store('photo', 'public');
+        }
+
+        $formFields['user_id'] = auth()->id();
+        $formFields['team_id'] = auth()->id();
+
+        Postcard::create($formFields);
+
+        return redirect('/postcards/manage')->with('message', 'Postcard created successfully!');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePostcardRequest $request)
-    {
-        //
+    // Show Edit Form
+    public function edit(Postcard $postcard) {
+        return view('postcards.edit', ['postcard' => $postcard]);
     }
 
     /**
@@ -39,30 +71,73 @@ class PostcardController extends Controller
      */
     public function show(Postcard $postcard)
     {
-        return view('postcards.show', compact('postcard'));
+        //Check that resource is online
+        $online = Postcard::where((Carbon::parse(date('Y-m-d H:s:i', strtotime('online_at')))
+                    ->diffInSeconds(Carbon::parse(date('Y-m-d H:s:i', strtotime('offline_at'))), false)), '>=', '0');   
+
+        if($online == "[]"){
+            $online = "";
+        }
+
+        //Confirm postcard not deleted
+        $model = Postcard::onlyTrashed()      
+                    ->where('id', '=', $postcard->id)
+                    ->get();
+        if($model == "[]"){
+            $model = "";
+        }
+
+        //Get postcard schema
+        $product = Postcard::findOrFail($postcard->id);
+        $schema = $product->getSchema();        
+        
+        if($online == ""){
+            abort_if(!$online, response(Redirect::to('/')
+                ->with('message', '410, Resource is offline!'), 410));
+        }
+
+        if($model != ""){
+            abort_if($model, response(Redirect::to('/')
+                ->with('message', '301, Postcard unavailable!'), 301));                     
+        }
+
+        return view('postcards.show', compact('postcard', 'schema'));         
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Postcard $postcard)
-    {
-        //
+    // Update Postcard Data
+    public function update(Request $request, Postcard $postcard) {
+               
+        $formFields = $request->validate([
+            'title' => 'required',            
+            'price' => 'required',            
+            'is_draft' => 'required'
+        ]);
+
+        if($request->hasFile('photo')) {
+            $formFields['photo'] = $request->file('photo')->store('photo', 'public');
+        }
+
+        $postcard->update($formFields);
+
+        return back()->with('message', 'Postcard updated successfully!');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePostcardRequest $request, Postcard $postcard)
-    {
-        //
+    // Delete Postcard
+    public function destroy(Postcard $postcard) {        
+        
+        if($postcard->photo && Storage::disk('public')->exists($postcard->photo)) {
+            Storage::disk('public')->delete($postcard->photo);
+        }
+        $postcard->delete();
+        return redirect('/postcards/manage')->with('message', 'Postcard deleted successfully');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Postcard $postcard)
-    {
-        //
-    }
+    // Manage Postcards
+    public function manage() {       
+        return view('postcards.manage', [
+            'postcards' => Postcard::latest()->filter(request(['search']))
+                        ->paginate(5)
+            ]);
+    }    
+
 }
--- app/Models/Postcard.php
+++ app/Models/Postcard.php
@@ -9,6 +9,24 @@ class Postcard extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'title',            
+        'price',
+        'online_at',
+        'offline_at',
+        'is_draft',
+        'user_id',
+        'team_id',
+        'photo'
+    ];
+
+    public function scopeFilter($query, array $filters) {
+        
+        if($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%');
+        }
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
