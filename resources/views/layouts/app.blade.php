515a002cdcb7eecb4fd7f23ae02c9ca77ac24cee
pshitote@gmail.com <pshitote@gmail.com>
2023-06-29 22:45:04 +0300

    Added canonical tag to the layouts of app.blade, guest.blade and layout.blade under components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
+        <!-- Canonical url -->
+        <link rel="canonical" href="{{request()->fullUrl()}}">
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
