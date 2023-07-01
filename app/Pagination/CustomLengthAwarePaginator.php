68bd268295a85781849931dbc9c88e6667f6e532
pshitote@gmail.com <pshitote@gmail.com>
2023-07-01 20:55:27 +0300

    modified pagination link to page=1 using customlengthawarepaginator under App/Pagination/CustomLengthAwarePaginator and AppServiceProvider.

--- app/Pagination/CustomLengthAwarePaginator.php
+++ app/Pagination/CustomLengthAwarePaginator.php
@@ -3,8 +3,8 @@
 namespace App\Pagination;
 
 use Illuminate\Pagination\LengthAwarePaginator;
-use Illuminate\Pagination\UrlWindow;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 
 class CustomLengthAwarePaginator extends LengthAwarePaginator
 {
@@ -16,12 +16,24 @@ class CustomLengthAwarePaginator extends LengthAwarePaginator
      */
     public function url($page)
     {
-        if ($page <= 1 && $this->path !== '/') {
-            return $this->path;
+        if ($page <= 0) {
+            $page = 1;
         }
 
-        return parent::url($page);
+        // If we have any extra query string key / value pairs that need to be added
+        // onto the URL, we will put them in query string form and then attach it
+        // to the URL. This allows for extra information like sortings storage.
+        $parameters = ($page > 1) ? [$this->pageName => $page] : [];
+
+        if (count($this->query) > 0) {
+            $parameters = array_merge($this->query, $parameters);
+        }
+
+        return $this->path()
+            . (count($parameters) > 0
+                ? (Str::contains($this->path(), '?') ? '&' : '?')
+                : '')
+            . Arr::query($parameters)
+            . $this->buildFragment();
     }
 }
-
-?>
